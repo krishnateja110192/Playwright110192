5b01dd019126cec2c503df62c18f6694576cc107
krishnateja110192 <krishnateja1101@gmail.com>
2025-06-23 18:08:30 +0530

    Update haviconnect.spec.js

--- tests/haviconnect.spec.js
+++ tests/haviconnect.spec.js
@@ -3,10 +3,10 @@ import { test, expect } from '@playwright/test';
 test('havi test', async ({ page }) => {
     test.setTimeout(120000);
   await page.goto('https://www.haviconnect.com');
-  await page.getByRole('textbox', { name: 'Please enter connect user id' }).fill('ahi.de.mcd@haviconnect.com');
+  await page.getByRole('textbox', { name: 'Please enter connect user id' }).fill('xxxxxxxxx');
   await page.getByRole('button', { name: 'Continue with Connect Account' }).click();
   await page.getByRole('textbox', { name: 'Enter the password for ahi.de' }).click();
-  await page.getByRole('textbox', { name: 'Enter the password for ahi.de' }).fill('Hav!@c0nnect');
+  await page.getByRole('textbox', { name: 'Enter the password for ahi.de' }).fill('xxxxxxxxx');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByRole('button', { name: 'Yes' }).click();
   await page.getByRole('textbox', { name: 'Search' }).click();
